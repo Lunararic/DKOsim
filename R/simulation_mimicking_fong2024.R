9976d2591a84f356f560a7bf202a9ba43fb5dbd1
Yue Gu <yue.gu@columbia.edu>
2025-09-05 19:28:35 -0500

    Update analysis to simulation mimicking Fong-2024

--- R/simulation_mimicking_fong2024.R
+++ R/simulation_mimicking_fong2024.R
@@ -1,8 +1,8 @@
 # Title: Double-CRISPR Knockout Simulation (DKOsim) - Mimicking Fong-2024 A549 Data
 # Author: Yue Gu, Luis Leon Novelo, John Paul Shen, Traver Hart
-# Fong-2024 Data Reference: Fong, S.H., Kuenzi, B.M., Mattson, N.M. et al. A multilineage screen identifies actionable synthetic lethal interactions in human cancers. Nat Genet 57, 154–164 (2025). https://doi.org/10.1038/s41588-024-01971-9
+# Fong-2024 Reference: Fong, S.H., Kuenzi, B.M., Mattson, N.M. et al. A multilineage screen identifies actionable synthetic lethal interactions in human cancers. Nat Genet 57, 154–164 (2025). https://doi.org/10.1038/s41588-024-01971-9
 
-# Load necessary libraries
+# Setup libraries and parallel computing background
 if (!requireNamespace("foreach", quietly = TRUE)) install.packages("foreach")
 if (!requireNamespace("doParallel", quietly = TRUE)) install.packages("doParallel")
 if (!requireNamespace("dplyr", quietly = TRUE)) install.packages("dplyr")
@@ -15,7 +15,13 @@ if (!requireNamespace("gtools", quietly = TRUE)) install.packages("gtools")
 if (!requireNamespace("plyr", quietly = TRUE)) install.packages("plyr")
 if (!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse")
 if (!requireNamespace("data.table", quietly = TRUE)) install.packages("dplyr")
+# package for parallel computing 
+if (!requireNamespace("foreach", quietly = TRUE)) install.packages("foreach")
+if (!requireNamespace("doParallel", quietly = TRUE)) install.packages("doParallel")
+if (!requireNamespace("gtools", quietly = TRUE)) install.packages("gtools")  # For rdirichlet()
+if (!requireNamespace("data.table", quietly = TRUE)) install.packages("dplyr")
 
+# Load libraries
 library(xtable)
 library(MCMCpack)
 library(entropy)
@@ -28,15 +34,21 @@ library(foreach)
 library(extraDistr)
 library(tidyverse)
 library(data.table)
-source(dkosim.R)
-### Major Modifications Before Each Run
-### 1.sample_name
-### 2.parameter specifications: simulation settings
-### 3.codes: (1)proportion of each gene class; (2)gene class distribution w/ means and std. dev
+library(foreach)
+library(doParallel)
+library(gtools)  # For rdirichlet()
+library(dplyr)
+library(data.table)
+set.seed(888) # random seed
 
-## initialize parameters
-sample_name = "DKO_simulation_hpc_v8_guide_250x3x3x2_1000x_run139"
-set.seed(888) # tune the random seeds to ensure the initialized guide-level counts are exceeding the same initialized bottleneck size if needed
+source("./R/dkosim.R")
+### Major Modifications Before Each Run
+### 1. Simulation Name: sample_name
+### 2. Simulation Settings: initialized parameter
+
+################################### Initialize Parameters ##########################################################
+# name of the simulation run
+sample_name = "DKOsim_mimic_fong2024"
 
 ## initialized library parameters
 coverage = 1000 # coverage: cell representation per guide
@@ -46,70 +58,6 @@ n_gene_pairs = n * (n-1) / 2 + n  # number of unique gene pairs (both SKO and DK
 n_construct = (n*n_guide_g) * ((n-1)*n_guide_g) / 2 + n*n_guide_g  # total number of constructs
 library_size = n_construct * coverage # number of total cells in the initialized gene-level library
 sd_freq0 = 1/(2*qnorm(0.90)) # std dev of initialized counts distribution
-
-## Run Simulations
-# Setup parallel computing background
-if (!requireNamespace("foreach", quietly = TRUE)) install.packages("foreach")
-if (!requireNamespace("doParallel", quietly = TRUE)) install.packages("doParallel")
-if (!requireNamespace("gtools", quietly = TRUE)) install.packages("gtools")  # For rdirichlet()
-if (!requireNamespace("data.table", quietly = TRUE)) install.packages("dplyr")
-## Load libraries
-library(foreach)
-library(doParallel)
-library(gtools)  # For rdirichlet()
-library(dplyr)
-library(data.table)
-
-## Use all available cores
-num_cores <- parallel::detectCores()
-cl <- makeCluster(num_cores)
-registerDoParallel(cl)
-start_time <- proc.time()
-
-# Gene-level Cell Library
-##initialize shared gene-level cell library, same across replicates
-cell_lib0 <- initialize_gene_cell_lib0()
-
-# Guide-level Cell Library
-## initialize independent guide-level cell library by replicates
-cell_lib_guide00 <- foreach(replicate = c("repA", "repB"), .packages = c("dplyr", "data.table", "truncnorm"), .combine = "list") %dopar% {
-  initialize_guide_cell_lib0(replicate, cell_lib0)
-}
-## extract from the list into separated initialized guide-level cell library
-cell_lib_guide0_A <- cell_lib_guide00[[1]]
-cell_lib_guide0_B <- cell_lib_guide00[[2]]
-
-# Define phenotypes and GI
-cell_lib_guide0 <- foreach(replicate = list(cell_lib_guide0_A, cell_lib_guide0_B), .packages = c("gtools", "dplyr", "foreach"), .combine = "list") %dopar% {
-  define_phenotype_gi(replicate)
-}
-## extract initialized guide-level cell library with all elements
-cell_lib_guide0_A <- cell_lib_guide0[[1]]
-cell_lib_guide0_B <- cell_lib_guide0[[2]]
-## check the initialized guide-level data
-print(cell_lib_guide0_A)
-print(cell_lib_guide0_B)
-
-# Run Simulations
-## Optimization: Run guide-level simulation for both replicates in parallel
-results <- foreach(replicate = c("repA", "repB"), .packages = c("dplyr", "foreach", "doParallel", "tidyr", "data.table", "extraDistr"), .combine = "c") %dopar% {
-  if (replicate == "repA") {
-    run_replicate("repA", cell_lib_guide0_A)
-  } else {
-    run_replicate("repB", cell_lib_guide0_B)
-  }
-  return(paste(replicate, "completed"))
-}
-## Print the collected results
-print(results)
-
-# Stop the cluster and collect running time in seconds
-stopCluster(cl)
-end_time <- proc.time()
-elapsed_time <- end_time - start_time; elapsed_time
-# check used cores and running time
-print(paste("number of cores", num_cores))
-print(paste("Run Time (hrs): ", elapsed_time["elapsed"]/3600))
 
 moi = 0.3 # moi
 moi_pois = dpois(1, moi) # get the number of viral particles delivered per cell during transfection from Poisson(moi) to calculate resampling size
@@ -123,7 +71,7 @@ mode = "CRISPRn" # CRISPR mode: use CRISPRn-100%Eff for need 100% efficient guid
 ### % of theoretical phenotype to each gene class - add up to 1
 pt_neg = 64/246 # % negative phenotype (essential)
 pt_pos = 0 # % positive phenotype (non-essential)
-pt_wt = 178/246 # % wt
+pt_wt = 178/246 # % wild-type
 pt_ctrl = 4/246 # % non-targeting control
 ### mean and std dev of theoretical phenotypes
 mu_neg = -0.03 # mean: negative phenotype
@@ -140,4 +88,10 @@ n.bottlenecks = 1 # how many times do we encountering bottlenecks?
 n.iterations = 30 # assuming a maximum of 30 doubling cycles if we didnt encounter bottleneck
 resampling = round(moi_pois * bottleneck)# determine resampling size based on moi and bottleneck size
 
-
+################################### Run Simulations ##########################################################
+dkosim(sample_name, 
+       coverage, n, n_guide_g, n_gene_pairs, n_construct, library_size, sd_freq0,
+       moi, moi_pois, p_gi, sd_gi, p_high, mode,
+       pt_neg, pt_pos, pt_wt, pt_ctrl,
+       mu_neg, sd_neg, mu_pos, sd_pos, sd_wt,
+       bottleneck, n.bottlenecks, n.iterations, resampling)
